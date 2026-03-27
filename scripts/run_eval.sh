1d61c121b04943ac38be3a91e12adc5922f0036f
Aniket Junghare <aniketjunghare.iitb@gmail.com>
2026-03-27 20:55:29 +0530

    Update run_eval.sh

--- scripts/run_eval.sh
+++ scripts/run_eval.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:1
+#SBATCH --partition=dgx
+#SBATCH --qos=dgx
+#SBATCH --time=6-00:00:00
+#SBATCH --exclude=cn15-dgx
+#SBATCH --output=rodrigo_evaluation.%j.out
+#SBATCH --error=rodrigo_evaluation.%j.err
 
+
+# Load conda and activate env
+source /home/vis-comp/aniketjunghare/anaconda3/etc/profile.d/conda.sh
+conda activate gsoc_26
+
+# Run evaluation
+python -u evaluate.py \
+  --model-dir models/Qwen2.5-VL-7B-Instruct \
+  --lora models/qwen2.5-vl-ocr-lora-handwritten/final \
+  --image-dir data/Rodrigo_eval/Rodrigo_Images \
+  --gt-dir data/Rodrigo_eval/Rodrigo_Transcriptions \
+  --output-csv results/Rodrigo_evaluation_results.csv
